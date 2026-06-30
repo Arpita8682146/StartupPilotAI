8c9b48e4d8c5859d590bf248433018762410c2f4
Arpita Singh <arpitasingh212006@gmail.com>
2026-06-30 13:41:14 +0530

    Add PDF upload functionality

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
 import streamlit as st
+import fitz
 
 st.title("StartupPilotAI")
 
-st.write("Your AI Co-Founder for Startup Success")
+uploaded_file = st.file_uploader(
+    "Upload PDF",
+    type=["pdf"]
+)
 
-st.success("Day 2 Setup Successful")
+if uploaded_file:
+
+    pdf = fitz.open(
+        stream=uploaded_file.read(),
+        filetype="pdf"
+    )
+
+    text=""
+
+    for page in pdf:
+
+        text += page.get_text()
+
+    st.success(
+        "PDF processed successfully"
+    )
+
+    st.text_area(
+        "Extracted Text",
+        text,
+        height=400
+    )
